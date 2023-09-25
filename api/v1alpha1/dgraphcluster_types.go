f0e68b4ef77b5fd95a818bcbef0570b12effe479
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2023-09-26 00:49:31 +0700

    initial impl of DgraphCluster api

--- api/v1alpha1/dgraphcluster_types.go
+++ api/v1alpha1/dgraphcluster_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -25,17 +26,76 @@ import (
 
 // DgraphClusterSpec defines the desired state of DgraphCluster
 type DgraphClusterSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Size is the expected size of the Dgraph cluster.
+	// The valid range of the size is from 1 to 7.
+	Size int `json:"size"`
 
-	// Foo is an example field of DgraphCluster. Edit dgraphcluster_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Repository is the name of the repository that hosts
+	// Dgraph container images.
+	//
+	// By default, it is `docker.io/dgraph/dgraph`.
+	Repository string `json:"repository,omitempty"`
+
+	// Version is the expected version of the Dgraph cluster.
+	//
+	// Only released versions are supported: https://github.com/dgraph-io/dgraph/releases
+	//
+	// If version is not set, the default is "v23.1.0".
+	Version string `json:"version,omitempty"`
+
+	// AlphaPod defines the policy to create pod for the alpha pod.
+	//
+	// Updating Pod does not take effect on any existing alpha pods.
+	AlphaPod *PodPolicy `json:"alphaPod,omitempty"`
+	
+	// ZeroPod defines the policy to create pod for the zero pod.
+	//
+	// Updating Pod does not take effect on any existing zero pods.
+	ZeroPod *PodPolicy `json:"zeroPod,omitempty"`
+}
+
+// PodPolicy defines the policy to create pod for the Dgraph container.
+type PodPolicy struct {
+	// Shortest path for configuring the pod.
+	// TO-DO: Abstract this out better.
+	v1.PodSpec `json:",inline"`
+	Labels map[string]string `json:"labels,omitempty"`
+	PersistentVolumeClaimSpec *v1.PersistentVolumeClaimSpec `json:"persistentVolumeClaimSpec,omitempty"`
+}
+
+type ClusterPhase string
+
+const (
+	ClusterPhaseNone     ClusterPhase = ""
+	ClusterPhaseCreating              = "Creating"
+	ClusterPhaseRunning               = "Running"
+	ClusterPhaseFailed                = "Failed"
+)
+
+type MembersStatus struct {
+	// Ready are the dgraph nodes that are ready to serve requests
+	// The member names are the same as the dgraph pod names
+	Ready []string `json:"ready,omitempty"`
+	// Unready are the dgraph nodes not ready to serve requests
+	Unready []string `json:"unready,omitempty"`
 }
 
 // DgraphClusterStatus defines the observed state of DgraphCluster
 type DgraphClusterStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Phase is the cluster running phase
+	Phase  ClusterPhase `json:"phase"`
+
+	// Size is the current size of the cluster
+	Size int `json:"size"`
+
+	Alphas MembersStatus `json:"alphas"`
+	Zeros MembersStatus `json:"zeros"`
+
+	// CurrentVersion is the current cluster version
+	CurrentVersion string `json:"currentVersion"`
+	// TargetVersion is the version the cluster upgrading to.
+	// If the cluster is not upgrading, TargetVersion is empty.
+	TargetVersion string `json:"targetVersion"`
 }
 
 //+kubebuilder:object:root=true
